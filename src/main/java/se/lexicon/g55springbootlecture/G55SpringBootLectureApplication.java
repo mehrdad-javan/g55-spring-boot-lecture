4e41df1d2212ab62dcf390c35b193b9ecf60b2c8
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-06-04 10:00:47 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g55springbootlecture/G55SpringBootLectureApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g55springbootlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G55SpringBootLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G55SpringBootLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g55springbootlecture/G55SpringBootLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g55springbootlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G55SpringBootLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
